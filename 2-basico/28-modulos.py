1c360c604ca9d6354fa44096fe0103ea5160e91c
wagnerbizarro <wagnerbzr@gmail.com>
2023-08-29 11:28:07 -0300

    Feat: Feito ate modulos

---
+++ 2-basico/28-modulos.py
@@ -0,0 +1,14 @@
+import utilitarios as ut
+
+print(ut.soma(1,2,3))
+
+print(ut.mult(3,2,2))
+
+print(ut.isPalindromo('abc'))
+
+print(ut.isPalindromo('abccba'))
+
+print(ut.div(10,5))
+
+
+
---
+++ 2-basico/arquivo.py
@@ -0,0 +1,5 @@
+def number(a):
+    return "A idade é: ", a
+
+def string(name):
+    return "O nome é: ", name
